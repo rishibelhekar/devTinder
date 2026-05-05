c596f222822985af96364aa972a0ae2f12d61faf
rishibelhekar <rishi.9684@gmail.com>
2026-05-05 16:38:27 +0530

    postman and http explore

--- src/app.js
+++ src/app.js
@@ -6,10 +6,16 @@ app.use("/test", (req, res) => {
     res.send("Hello Word test page")
 })
 
+app.use("/hello/2", (req, res) => {
+    res.send("Nested Route")
+})
+
 app.use("/hello", (req, res) => {
     res.send("this is hello page route")
 })
 
+
+// Start server
 app.listen(3000, () => {
     console.log("server started...")
 })
@@ -18,6 +24,19 @@ app.use("/check", (req, res) => {
     res.send("check page view")
 })
 
+app.get("/about", (req, res) => {
+    res.send("get data from about")
+})
+
+app.post("/about", (req, res) => {
+    res.send("Post data on about")
+})
+
+app.delete("/about", (req, res) => {
+    res.send("delete data on about")
+})
+
 app.use("/", (req, res) => {
     res.send("Dashboard page")
 })
+
